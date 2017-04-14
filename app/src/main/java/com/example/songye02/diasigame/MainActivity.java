fb50bd1f959cac4e2b9dd43f97453fc3c9ee8616
songye02 <songye02@baidu.com>
2017-04-14 19:03:37 +0800

    各种下笔写的测试

--- app/src/main/java/com/example/songye02/diasigame/MainActivity.java
+++ app/src/main/java/com/example/songye02/diasigame/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.songye02.diasigame;
 
+import com.example.songye02.diasigame.test.MySurfaceView;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //设置无标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //设置全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(new MySurfaceView(this));
+//        LinearLayout layout = (LinearLayout)findViewById(R.id.main_view);
+//        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(0,0);
+//        params.gravity = Gravity.RIGHT;
+//        layout.addView(new MySurfaceView(this),params);
     }
+
+
 }
---
+++ app/src/main/java/com/example/songye02/diasigame/test/RotateTextView.java
@@ -0,0 +1,31 @@
+package com.example.songye02.diasigame.test;
+
+import android.content.Context;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by songye02 on 2017/4/14.
+ */
+
+public class RotateTextView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public RotateTextView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
